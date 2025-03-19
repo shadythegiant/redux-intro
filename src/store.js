4b04d8250c66df5d129d44c8a3eba59ea0a51bb4
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-03-19 22:02:49 +0000

    :octocat:

--- src/store.js
+++ src/store.js
@@ -1,24 +1,50 @@
+import { createStore } from "redux";
+
 const intialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state, action) {
+function reducer(state = intialState, action) {
   switch (action.type) {
-    case "account/deposit ":
+    case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
 
-    case "account/withdraw  ":
+    case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
 
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
 
-      case ""
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
 
     default:
       return state;
   }
 }
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 500 });
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 400, purpose: "bying  a car" },
+});
+console.log(store.getState());
